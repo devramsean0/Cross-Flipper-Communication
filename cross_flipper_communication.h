819c82b6eb02e3c2ef2552e317f8379f480a8370
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-10-25 15:32:50 +0100

    Implement SubGHz select mode

---
+++ cross_flipper_communication.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <gui/modules/text_box.h>
+#include <gui/modules/widget.h>
+#include <gui/modules/submenu.h>
+#include <gui/modules/text_input.h>
+#include <gui/scene_manager.h>
+#include <gui/view_dispatcher.h>
+
+typedef struct {
+    SceneManager* scene_manager;
+    ViewDispatcher* view_dispatcher;
+    TextBox* text_box;
+    Widget* widget;
+    Submenu* submenu;
+    TextInput* text_input;
+    float frequency;
+} App;
